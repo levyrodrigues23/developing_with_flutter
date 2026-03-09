c03a001c8015f6e2dc46af9474b4d86d4df2d633
levyrodrigues23 <joselevyrodrigues23@gmail.com>
2026-03-09 18:01:40 -0300

    feat: entendendo statelessWidget e statefullWidget

---
+++ lib/home_page_statefull.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+
+class HomePageStatefull extends StatefulWidget {
+  const HomePageStatefull({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageStateFullState();
+  }
+}
+
+class _HomePageStateFullState extends State<HomePageStatefull> {
+  String texto = 'estou no statefullwidget';
+
+  @override
+  void initState() {
+    super.initState();
+    texto = 'texto alterado pelo initstate';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("buildando o home page statefull");
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Text(texto),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                texto = "eu alterei o texto agora";
+              });
+            },
+            child: Text('alterar texto'),
+          ),
+        ],
+      ),
+    );
+  }
+}
