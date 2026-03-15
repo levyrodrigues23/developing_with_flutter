2864075e41b3003fe95fbb1232c93920d6e44692
levyrodrigues23 <joselevyrodrigues23@gmail.com>
2026-03-14 22:37:58 -0300

    feat: refactor ImagesPage to use BoxDecoration for image display

--- lib/images/images_page.dart
+++ lib/images/images_page.dart
@@ -13,20 +13,30 @@ class ImagesPage extends StatelessWidget {
             Container(
               width: 200,
               height: 200,
-              child: Image.asset(
-                'assets/teste.jpg',
-                fit: BoxFit
-                    .cover, // ele acaba distorcendo a imagem, ou seja, essa é a característica do fill
+
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/teste.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  "just a image",
+                  style: TextStyle(
+                    backgroundColor: Colors.white.withValues(alpha: 0.5),
+                    fontSize: 20,
+                  ),
+                ),
               ),
             ),
             Container(
               width: 200,
               height: 200,
-              child: Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDl6MYXHMPJg-VkRIL3oEKwpgSQ9JkUJxWuQ&s',
-                fit: BoxFit.fill
+              child: Image.network(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDl6MYXHMPJg-VkRIL3oEKwpgSQ9JkUJxWuQ&s',
+                fit: BoxFit.fill,
               ),
-              
-              
             ),
           ],
         ),
